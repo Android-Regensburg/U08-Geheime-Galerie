af34ace1cdddfc3031394c0c0133be52ce5aa6db
Pascal.Strobel <53040413+paschistrobel@users.noreply.github.com>
2021-06-03 13:55:51 +0200

    FileProvider aus Manifest entfernt

--- app/src/main/java/de/ur/mi/android/base/CreationActivity.java
+++ app/src/main/java/de/ur/mi/android/base/CreationActivity.java
@@ -1,9 +1,12 @@
 package de.ur.mi.android.base;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
